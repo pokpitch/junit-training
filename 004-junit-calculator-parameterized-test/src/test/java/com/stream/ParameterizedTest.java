e67492ef0e1f6f817ae016cca1822dba7c20aa97
Pokpitch Patcharadamrongkul <pokpitch@gmail.com>
2016-05-25 01:00:59 +0700

    add junit paramaterized test case

---
+++ 004-junit-calculator-parameterized-test/src/test/java/com/stream/ParameterizedTest.java
@@ -0,0 +1,38 @@
+package com.stream;
+
+import java.util.Arrays;
+import java.util.Collection;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.junit.runner.RunWith;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+@RunWith(value=Parameterized.class)
+public class ParameterizedTest {
+  private double expected;
+  private double valueOne;
+  private double valueTwo;
+
+  @Parameters
+  public static Collection<Integer[]> getTestParameters() {
+  	return Arrays.asList(new Integer[][] {
+      {2, 1, 1}, // expected, valueOne, valueTwo
+			{3, 2, 1}, // expected, valueOne, valueTwo
+      {4, 3, 1}, // expected, valueOne, valueTwo
+		});
+  }
+
+  public ParameterizedTest(double expected, double valueOne, double valueTwo) {
+		this.expected = expected;
+		this.valueOne = valueOne;
+    this.valueTwo = valueTwo;
+  }
+
+  @Test
+	public void sum() {
+    Calculator calc = new Calculator();
+    assertEquals(expected, calc.add(valueOne, valueTwo), 0);
+  }
+
+}
