2e2fced7353e356fc95c109ece8d6c1aa6dd86f3
Pokpitch Patcharadamrongkul <pokpitch@gmail.com>
2016-05-25 11:17:31 +0700

    add master test suite

---
+++ 005-junit-master-test-suite/src/test/java/com/stream/TestCaseA.java
@@ -0,0 +1,11 @@
+package com.stream;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class TestCaseA {
+	@Test
+	public void testA1() {
+		assertEquals("Dummy test-case", 1+1, 2);
+	}
+}
---
+++ 005-junit-master-test-suite/src/test/java/com/stream/TestCaseB.java
@@ -0,0 +1,11 @@
+package com.stream;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class TestCaseB {
+	@Test
+	public void testB1() {
+		assertTrue("Dummy test-case", true);
+	}
+}
