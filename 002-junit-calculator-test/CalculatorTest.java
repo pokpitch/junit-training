98cda326faa09c3e4ecb607df84499ea68689749
Pokpitch Patcharadamrongkul <pokpitch@gmail.com>
2016-05-26 23:33:01 +0700

    using Arrange-Act-Assert

--- 002-junit-calculator-test/CalculatorTest.java
+++ 002-junit-calculator-test/CalculatorTest.java
@@ -4,8 +4,19 @@ import org.junit.Test;
 public class CalculatorTest {
   @Test
   public void testAdd() {
+
     Calculator calculator = new Calculator();
-    double result = calculator.add(10, 50);
-    assertEquals(60, result, 0);
+
+    // Arrange
+    double firstNumber = 10;
+    double secondNumber = 50;
+    double expected = 60;
+
+    // Act
+    double result = calculator.add(firstNumber, secondNumber);
+
+    // Assert
+    assertEquals(expected, result, 0);
+    
   }
 }
